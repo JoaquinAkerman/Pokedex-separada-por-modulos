da120370b3dd0f02fbe003e75747e598cf6f5009
Joaquin Akerman <joaquin_akerman@hotmail.com>
2022-09-14 12:22:26 -0300

    refactor de código

--- modules/storage/storage.js
+++ modules/storage/storage.js
@@ -1,7 +1,10 @@
-export default function guardarPaginaEnLocalStorage(
-  dataDePaginaEnJson,
-  direccionDePagina,
-) {
+function guardarPaginaEnLocalStorage(dataDePaginaEnJson, direccionDePagina) {
   const dataDePaginaEnString = JSON.stringify(dataDePaginaEnJson);
   localStorage.setItem(direccionDePagina, dataDePaginaEnString);
 }
+
+function buscarEnLocalStorage(direccionPagina) {
+  const paginaEnLocalStorage = localStorage.getItem(direccionPagina);
+  return paginaEnLocalStorage;
+}
+export { guardarPaginaEnLocalStorage, buscarEnLocalStorage };
