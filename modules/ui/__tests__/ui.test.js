eeec07abe2a6654e2a0abb32f9460424883eefbc
Joaquin Akerman <joaquin_akerman@hotmail.com>
2022-11-02 09:38:13 -0300

    Agrega pruebas de UI

--- modules/ui/__tests__/ui.test.js
+++ modules/ui/__tests__/ui.test.js
@@ -24,15 +24,30 @@ test('prueba que la lista de pokemons se arme con 5 pokemons', () => {
   expect(document.querySelectorAll('button')).toHaveLength(5);
 });
 
-test('Prueba el paginador con 1 pagina, 10 pokemons, y que la pagina activa sea la 1', () => {
+test('Prueba el paginador con 2 paginas, 40 pokemons, que la pagina activa sea la 1 por default, y que boton anterior se oculte', () => {
   document.body.innerHTML = fixture;
-  crearPaginador(2, 10);
+  crearPaginador(2, 40);
   expect(document.querySelector('#paginador').innerHTML).toContain('pagina-activa');
   expect(document.querySelector('#pagina-activa').innerText).toBe(1);
   expect(document.querySelector('#pagina-activa').classList).toContain('page-link');
   expect(document.querySelector('#pagina-actual').innerText).toBe('Página 1');
   expect(document.querySelectorAll('.page-item')).toHaveLength(2);
+  expect(document.querySelector('#boton-anterior').classList).toContain('oculto');
   expect(document.querySelector('#cantidad-de-pokemones').innerHTML).toBe(
-    'Hay 10 Pokemones, selecciona uno para ver la info',
+    'Hay 40 Pokemones, selecciona uno para ver la info',
+  );
+});
+
+test('Prueba paginador con 3 paginas, 60 pokemons, que la pagina activa sea 3, y que boton siguiente se oculte', () => {
+  document.body.innerHTML = fixture;
+  crearPaginador(3, 60, 3);
+  expect(document.querySelector('#paginador').innerHTML).toContain('pagina-activa');
+  expect(document.querySelector('#pagina-activa').innerText).toBe(3);
+  expect(document.querySelector('#pagina-activa').classList).toContain('page-link');
+  expect(document.querySelector('#pagina-actual').innerText).toBe('Página 3');
+  expect(document.querySelectorAll('.page-item')).toHaveLength(3);
+  expect(document.querySelector('#boton-siguiente').classList).toContain('oculto');
+  expect(document.querySelector('#cantidad-de-pokemones').innerHTML).toBe(
+    'Hay 60 Pokemones, selecciona uno para ver la info',
   );
 });
